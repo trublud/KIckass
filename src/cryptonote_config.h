305d7c6b707a8dd0a225c9709ec464fa0d8039bd
trublud <xmindpingx@gmail.com>
2019-07-09 22:00:21 -0700

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -179,7 +179,7 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0x42 ,0x04, 0x20, 0x42 , 0x04, 0x20 , 0x42, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "";
+  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
   uint32_t const GENESIS_NONCE = 10042;
 
   namespace testnet
